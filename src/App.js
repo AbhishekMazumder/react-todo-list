d6c9a244fd41eb3b94f4fc9aa8340254c2c82dcf
abhishek <abhishekmazumder69@gmail.com>
2020-06-10 17:46:54 +0530

    App component setup with methods and state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,67 @@
-import React, { Component } from 'react'
-import uuid from 'uuid';
+import React, { Component } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import TodoInput from './components/TodoInput';
-import TodoList from './components/TodoList'
+import TodoList from './components/TodoList';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <TodoInput />
-        <TodoList />
-      </div>
-    )
-  }
+class App extends Component {
+	state = {
+		items: [
+			{ id: 1, title: 'wake up' },
+			{ id: 2, title: 'make breakfast' },
+		],
+		id: uuidv4(),
+		item: '',
+		editItem: false,
+	};
+
+	handleChange = (e) => {
+		console.log('handle change');
+	};
+
+	handleSubmit = (e) => {
+		console.log('handle submit');
+	};
+
+	clearList = () => {
+		console.log('clearList');
+	};
+
+	handleDelete = (e) => {
+		console.log(`delete `);
+	};
+
+	handleEdit = (e) => {
+		console.log(`edit`);
+	};
+
+	render() {
+		return (
+			<div className="container">
+				<div className="row">
+					<div className="col-10 mx-auto col-md-8 mt-4">
+						<h3 className="text-capitalize text-center">
+							Todo Input
+						</h3>
+					</div>
+				</div>
+				<TodoInput
+					item={this.state.item}
+					handleChange={this.handleChange}
+					handleSubmit={this.handleSubmit}
+					editItem={this.editItem}
+				/>
+				<TodoList
+					items={this.state.items}
+					clearList={this.clearList}
+          handleDelete={this.handleDelete}
+          handleEdit={this.handleEdit}
+				/>
+			</div>
+		);
+	}
 }
+
+export default App;
